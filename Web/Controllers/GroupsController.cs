ce3983dd0f084717cbe5758e7c9440ac44f1c9d2
Illia-creator <sie29092002@gmail.com>
2022-11-08 14:18:30 +0200

    add project

---
+++ Web/Controllers/GroupsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using Web.Models;
+
+namespace Web.Controllers
+{
+    [Route("groups")]
+    public class GroupsController : Controller
+    {
+        private static long currentGroupId = 1;
+        private static List<GroupViewModel> groups = new List<GroupViewModel> { new GroupViewModel { Id = 1, Name = "Make`s Group" } };
+
+        [HttpGet]
+        [Route("")]
+        public IActionResult Index()
+        {
+
+            return View(groups);
+        }
+    }
+}
---
+++ Web/Models/GroupViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Web.Models
+{
+    public class GroupViewModel
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+    }
+}
